6f610c0d24bedff5ce431441d2f49879b47ede4c
ishmamzarif <165466648+ishmamzarif@users.noreply.github.com>
2024-10-04 13:34:16 +0600

    Create 00_code_template.cpp

---
+++ 00_code_template.cpp
@@ -0,0 +1,53 @@
+/*
+    tzara_0x1d
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define FAST_IO ios_base::sync_with_stdio(false); cin.tie(NULL);
+
+typedef long long ll;
+typedef long long unsigned llu;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef pair<int, int> pii;
+
+#define fr(i, n) for (int i = 0; i < (n); i++)
+#define repu(i, a, b) for (int i = (a); i < (b); i++)
+#define rept(i, a, b) for (int i = (a); i <= (b); i++)
+#define repur(i, a, b) for (int i = (a); i > (b); i--)
+#define reptr(i, a, b) for (int i = (a); i >= (b); i--)
+
+#define pb push_back
+#define mp make_pair
+#define all(v) v.begin(), v.end()
+#define len(v) (int)(v.size())
+#define fir first
+#define sec second
+
+// ll gcd(ll a, ll b) {return (b == 0) ? a : gcd(b, a % b);}
+// ll lcm(ll a, ll b) {return (a*b)/gcd(a,b);}
+// ll check_power_of_two (ll n) { return (n & (n - 1));}
+
+const int MAX = 1e6;
+const ll MOD = 1e9+7LL;
+
+void solve() {
+    
+}
+
+
+int main (void) {
+    FAST_IO;
+
+    int t; cin >> t;
+
+    while (t--)
+        solve();
+
+    return 0;
+}
+
+
+    
